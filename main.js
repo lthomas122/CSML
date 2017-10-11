0e380fc99e4a5472a139dcabd0e6c337d804718f
lthomas122 <liamdanielthomas@gmail.com>
2017-10-12 00:25:31 +0100

    Fixed add colour button

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 function addColorPanel(type) {
-  //console.log(type);
-  var count = 0;
-  count += 1;
-  type = type.toString();
+  if (type == 'bg'){
+    addColorPanel.bgCalled++;
+    addColorPanel.timesCalled = addColorPanel.bgCalled;
+  }
+  else if (type == 'txt'){
+    addColorPanel.txtCalled++;
+    addColorPanel.timesCalled = addColorPanel.txtCalled;
+  }
   var x = $(".color-panels-" + type);
   var $input = "<div>Color Name: <input type='text' class='colorname' value='Dark'></div><div>Color Hex: <input type='text' class='jscolor' value='222222'></div>";
   x.append($input);
-  new jscolor($('.color-panels-' + type +' .jscolor')[count]);
-  console.log(count);
+  new jscolor($('.color-panels-' + type +' .jscolor')[addColorPanel.timesCalled]);
 }
+addColorPanel.bgCalled = 0;
+addColorPanel.txtCalled = 0;
 
 $(function(){
   $("button").click(function(e){
